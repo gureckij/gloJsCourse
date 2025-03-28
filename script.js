e08e54a16318040aa19536492047b835d391f930
Gureckij <guretsky.aleksei@yandex.ru>
2025-03-28 13:20:02 +0300

    присвоение значений переменным

---
+++ script.js
@@ -0,0 +1,9 @@
+let title = "Название моего проекта";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 5;
+let rollback = 10;
+let fullPrice = 100;
+let adaptive = true;
+
+alert('Это тестовое модальное окно!');
+console.log('Вывод сообщения в консоль!');
