e940f87a42bc8e91b36d6ea310c87b05452e5f40
Gureckij <guretsky.aleksei@yandex.ru>
2025-04-06 11:19:19 +0300

    Запрос у пользователя название проекта и сохранение названия в переменную

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let title = "Название моего проекта";
+let title;
 let screens = "Простые, Сложные, Интерактивные";
 let screenPrice = 5;
 let rollback = 10;
 let fullPrice = 100;
 let adaptive = true;
 
-alert('Это тестовое модальное окно!');
-console.log('Вывод сообщения в консоль!');
+title = prompt("Как называется ваш проект?");
+console.log(title);
 
 console.log(typeof title);
 console.log(typeof fullPrice);
